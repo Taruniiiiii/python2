af61419c60851f69da92b36640890483938da42d
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-08-20 15:18:58 -0400

    added conditional_statements.py

---
+++ conditional_statements.py
@@ -0,0 +1,47 @@
+#1
+"""
+def friends_in_trouble(j_angry, s_angry):
+    if(j_angry == True and s_angry == True):
+        return True
+    elif(j_angry == True and s_angry == False):
+        return False
+
+#2
+def checkOddEven(x):
+    if x%2==0:
+        return "Even"
+    else:
+        return "Odd"
+    
+#3 return True if  the words "cat" and "hat" appear same 
+# number of times in str, otherwise return False.
+def cat_hat(str):
+  if str.count("cat") == str.count("hat"):
+      return True
+  else:
+      return False
+
+#4
+a=int(input())
+if(a>100):
+    print("Big")
+else:
+    print("Number")
+
+##
+if age<=12:
+    print("Travel for free")
+else:
+    print("Pay for ticket")
+
+##
+age=25
+
+if age<=12:
+    print("child.")
+elif age<=19:
+    print("teen")
+elif age<=35:
+    print("Young adult")
+else:
+    print("adult")"""

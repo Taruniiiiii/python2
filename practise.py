308d246fc02cdaf93ff3ca5f90a2fb7cf7cbef62
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-08-24 13:08:08 -0400

    practise d3

--- practise.py
+++ practise.py
@@ -48,3 +48,15 @@ elif q.isdigit():
     print("digit")
 else:
     print("special char")
+
+#d=3
+n = int(input())
+ if n%2==0 and n>=2 and n<=5:
+        print("Not Weird")
+    elif n%2==0 and n>=6 and n<=20:
+        print("Weird")
+    elif n%2==0 and n>20:
+        print("Not Weird")
+    else:
+        print("Weird")
+

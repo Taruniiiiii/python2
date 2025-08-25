1083e11c6024ef41294325850c594276cb9ed9ae
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-08-25 13:54:04 -0400

    loops

--- pattern.py
+++ pattern.py
@@ -10,10 +10,17 @@ n=int(input())
 for i in range(0,n+1):
     for j in range(i):
          print("*",end="")
-    print()"""
+    print()
 
 #Write a program to print all odd numbers from 1 to n.
 n=int(input())
 for i in range(0,n,2):
      n=i+1
-     print(n)
+     print(n)"""
+
+#Write a program to find the factorial of a number.
+n=int(input())
+fact=1
+for i in range(1,n):
+    fact=fact*i
+    print(fact)

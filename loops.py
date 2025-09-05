847851266680c487b8878589d867ef7fa08cbb8d
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-09-05 05:46:50 -0400

    sep5

--- loops.py
+++ loops.py
@@ -1,9 +1,13 @@
+#sep5
 #two sum
-nums=[1,2,3,4,5,6]
+"""nums=[1,2,3,4,5,6]
 target=int(input())
 n=len(nums)
 for i in range(n):
        for j in range(i+1,n):
               if nums[i]+nums[j]==target:
-                   print([i,j])
-             
+                   print([i,j])"""
+#Multiplication Table
+num=int(input("enter a num u want to multiply: "))
+for i in range(1,11):
+      print(num,"X",i,"=",num*i)

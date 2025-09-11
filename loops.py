f892c3d88359954dcae159449345dbd365976285
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-09-11 14:13:27 -0400

    sep11

--- loops.py
+++ loops.py
@@ -317,11 +317,16 @@ for i in range(0,n):
     for j in range(i+1):
         print(i,end=" ")
     print()
-#otp
-0 
+#otp"""
+"""0 
 1 1
 2 2 2
 3 3 3 3
 4 4 4 4 4
 5 5 5 5 5 5
-6 6 6 6 6 6 6
+6 6 6 6 6 6 6"""
+n=int(input())
+for i in range(1,n):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()

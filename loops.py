570288635b43b95bf67cb5370a184073c8e25449
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-09-11 13:35:56 -0400

    sep11

--- loops.py
+++ loops.py
@@ -299,10 +299,16 @@ for i in range(0, n):
 * * 
 * * *
 * * * *
-* * * * *"""
+* * * * *
 
 n=5
 for i in range(n,0,-1):
     for j in range(i):
         print("*",end=" ")
     print()
+    #otp
+* * * * * 
+* * * * 
+* * *
+* *
+*"""

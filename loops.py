15cd4732a1a0eea46eed0e35b1f4fc8cc6ea04af
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-09-14 13:19:59 -0400

    sep

--- loops.py
+++ loops.py
@@ -365,12 +365,12 @@ for i in range(n-1,0,-1):
  *********
   *******
    *****
-    ***"""
+    ***
      *
 #leetcode
 X=0
         for val in operations:
-            if "++" in val:
+            if "++" in val:"""
                 X+=1
             else:
                 X-=1

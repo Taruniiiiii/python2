621402f09bfae39b155feb0dfdd222b6b2548bb2
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-09-16 13:08:24 -0400

    sep16

---
+++ functions.py
@@ -0,0 +1,19 @@
+#syntax:
+"""
+def function_name(parameters):
+    #function body
+    """
+#ex.1
+def function_name():
+    print("I'm taruniii")
+function_name()#calling function
+
+#ex.2
+def evenOdd(x):
+    if (x%2==0):
+        return "Even"
+    else:
+        return "Odd"
+
+print(evenOdd(999))
+print(evenOdd(8))

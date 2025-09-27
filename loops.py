98d8173fe06814911ef24eff5cea39510573fbdf
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-09-27 09:17:50 -0400

    sep

--- loops.py
+++ loops.py
@@ -393,8 +393,8 @@ num = 1 # starting number
 for i in range(1, n+1):  # rows
     for j in range(i):   # print i numbers in that row
         print(num, end=" ")
-        num += 1"""
-    print()  # new line after each row
+        num += 1
+    print()  # new line after each row"""
 
 #leetcode
     def getConcatenation(self, nums: List[int]) -> List[int]:

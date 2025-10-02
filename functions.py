d8894ab37c027aadd6261c532162915de6b97ca1
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-02 14:33:31 -0400

    sep

--- functions.py
+++ functions.py
@@ -223,3 +223,12 @@ def fact(n):
     return fact
 print(fact(5))
 print(fact(7))
+
+#Sum of Digits of a Number
+def sum_of_digits(n):
+    sum=0
+    while n>0:
+        digit=n%10
+        sum=sum+digit
+        n=n//10
+    return sum

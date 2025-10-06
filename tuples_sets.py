d4785f1f0b70c5c5bec29f8806a027bf01550459
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-06 12:44:24 -0400

    sep

--- tuples_sets.py
+++ tuples_sets.py
@@ -3,11 +3,22 @@
 """fav_fruits=("apple","pineapple","kiwi")
 for fruits in fav_fruits:
     print(fruits)"""
+
 #Find the length of a tuple (4, 5, 6, 7, 8, 9).
 num=(4,5,6,7,8,9)
 print(len(num))
+
 #Check whether an element (like 7) exists in a tuple.
 if 17 in num:
     print("exists in tuple")
 else:
     print("does not exist in tuple")
+
+#Convert a tuple (10, 20, 30, 40) to a list, change the 
+# second element to 99, and convert it back to a tuple.
+tup=(10,20,30,40)
+li=list(tup)
+li[1]=99
+li[2]=30
+tup=tuple(li)
+print(tup)

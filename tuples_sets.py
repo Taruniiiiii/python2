d2a644c835cbbc7f35168cf32e3dc85b61ab3f91
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-10 14:22:17 -0400

    oct

--- tuples_sets.py
+++ tuples_sets.py
@@ -101,14 +101,14 @@ print(result)
 
 #reverse a tuple without using the reversed()
 ex=(1, 2, 3, 4)
-print(ex[::-1])
+print(ex[::-1])"""
 
 #Find Second Largest Number
 ex2=(5, 7, 2, 9, 1)
 lis=list(ex2)
 lis.sort()
 print(lis)
-print(lis[-2],"is the second highest num in ex2")""""
+print(lis[-2],"is the second highest num in ex2")
 
 #Multiply All Numbers in a Tuple
 ex3=(1, 2, 3, 4)

791e5a66ee990511bf2946a47536846577416825
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-12 14:11:02 -0400

    oct

--- built_in_functions.py
+++ built_in_functions.py
@@ -1,3 +1,12 @@
 #Use len() to find how many characters are in "Python".
 word="python"
 print(len(word))#6
+#find max
+num=(5, 10, 15, 20)
+print(max(num),"is max")
+print(sum(num),"is sum of num")
+
+#convert list to tuple
+li=[1, 2, 3, 4]
+tup=tuple(li)
+print(tup)

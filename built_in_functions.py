6bb8707d5f6a6e47556284875233417424976bc6
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-12 12:37:53 -0400

    oct

---
+++ built_in_functions.py
@@ -0,0 +1,3 @@
+#Use len() to find how many characters are in "Python".
+word="python"
+print(len(word))
--- pattern.py
+++ pattern.py
@@ -2,14 +2,14 @@
 n=int(input())
 for i in range(n):
     for j in range(n):
-         print("*",end="")
+        print("*",end="")
     print()
     """
 #2 right-angled triangle of stars
 n=int(input())
 for i in range(0,n+1):
     for j in range(i):
-         print("*",end="")
+        print("*",end="")
     print()
 
 #Write a program to print all odd numbers from 1 to n.

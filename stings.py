f4b175d845e7eab9c9345b64be7c3533b7d46f99
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-15 14:29:35 -0400

    oct

---
+++ stings.py
@@ -0,0 +1,21 @@
+"""| Function                        | Description             | Example                        |
+| ------------------------------- | ----------------------- | ------------------------------ |
+| `len()`                         | Returns length          | `len("hello") → 5`             |
+| `.upper()`                      | Convert to uppercase    | `"hi".upper() → 'HI'`          |
+| `.lower()`                      | Convert to lowercase    | `"Hi".lower() → 'hi'`          |
+| `.title()`                      | First letter capital    | `"hello world".title()`        |
+| `.strip()`                      | Removes spaces          | `"  hi  ".strip()`             |
+| `.replace(a, b)`                | Replace characters      | `"apple".replace("a", "A")`    |
+| `.split()`                      | Split into list         | `"a,b,c".split(",")`           |
+| `.join()`                       | Join elements           | `" ".join(["Hello", "World"])` |
+| `.count()`                      | Count occurrences       | `"banana".count("a")`          |
+| `.find()`                       | Find index of substring | `"python".find("t")`           |
+| `.startswith()` / `.endswith()` | Check beginning/end     | `"hello".startswith("he")`     |
+"""
+#f string
+name="Taruni"
+age=19
+education="MRU"
+department="ds"
+year=3
+print(f"My name is {name} and I am {age} years old. Iam a {year}rd year student at{education}")

95775d4205e6cd249b402ecd31ac34bb7c8b2550
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-18 14:44:19 -0400

    oct

--- stings.py
+++ stings.py
@@ -51,7 +51,7 @@ for i in word:
     else:
         print("invalid char")
 print(vowels_count,"vowels count")
-print(consonant_count,"consonant count")"""
+print(consonant_count,"consonant count")
 
 #reverse sentence 
 sentence="hi im taruni"
@@ -76,3 +76,23 @@ for ch in sent:
     if ch not in li:
         clear+=ch
 print(clear)
+"""
+#Count Uppercase, Lowercase, Digits, and Special Characters
+s=str(input())
+upper=0
+lower=0
+digit=0
+special=0
+for ch in s:
+    if ch.isupper():
+        upper+=1
+    elif ch.islower():
+        lower+=1
+    elif ch.isdigit():
+        digit+=1
+    else:
+        special+=1
+print(upper,"upper")
+print(lower,"lower")
+print(digit,"digit")
+print(special,"special")

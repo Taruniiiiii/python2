48e6dc798b1939b049d1f080159cf108e382a2ac
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-23 13:23:22 -0400

    oct

---
+++ dict.py
@@ -0,0 +1,8 @@
+#Python dictionary is a data structure that stores the value in key: value pairs.
+#ex
+d1={1:'ish',2:'soh',3:'tar'}
+print(d1)
+
+# create dictionary using dict() constructor
+d2=dict(a="tan",b="adh",c="sah")
+print(d2)

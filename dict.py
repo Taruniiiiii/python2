68414602ba58d8806972515c29131e39148fdbd9
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-10-24 13:35:16 -0400

    oct

--- dict.py
+++ dict.py
@@ -15,7 +15,7 @@ print(d[(1,2)])
 
 # Access using get()
 print(d.get("name"))
-"""
+
 # Adding a new key-value pair in the end
 d1["age"] = 22
 print(d1)
@@ -23,11 +23,11 @@ print(d1)
 d1[1] = "man"
 print(d1)
 
-del: Removes an item by key.
+"""del: Removes an item by key.
 pop(): Removes an item by key and returns its value.
 clear(): Empties the dictionary.
-popitem(): Removes and returns the last key-value pair."""
-
+popitem(): Removes and returns the last key-value pair.
+"""
 # Using del to remove an item
 del d1["age"]
 print(d1)
@@ -44,3 +44,16 @@ print(f"Key:{key},Value:{val}")
 # Clear all items from the dictionary
 d2.clear()
 print(d2)
+d = {1:'MRU',2:'MRCET','CMR':2025}
+
+# Iterate over keys
+for key in d:
+    print(key)#print just keys like here..1,2,cmr
+
+# Iterate over values
+for value in d.values():
+    print(value)#prints values liike  mru,mrcet,2025
+
+# Iterate over key-value pairs
+for key, value in d.items():#prints keys and values both
+    print(f"{key}: {value}")

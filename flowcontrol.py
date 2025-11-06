34ebdcd229f824d5892a2dde9d36dbe94b6c3106
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-06 12:54:55 -0500

    nov

--- flowcontrol.py
+++ flowcontrol.py
@@ -82,8 +82,8 @@ def printIncreasingPower(x):
 #  the neg function. If positive, print numbers from n-1 to 0 by
 # subtracting 1 from n in the pos function.
 def pos(n):
-    for i in range(n-1,-1,-1):
-        print(i,end=" ")"""
+    for i in range(n-1,-1,-1):"""
+        print(i,end=" ")
         
     
 def neg(n):

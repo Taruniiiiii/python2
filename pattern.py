e7b696e7f556d9f4a5e90bba6f67f9bf6564b3da
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-07 12:38:33 -0500

    nov

--- pattern.py
+++ pattern.py
@@ -3,7 +3,8 @@ n=int(input())
 for i in range(n):
     for j in range(n):
         print("*",end="")
-    print()"""
+    print()
+    
 #2 right-angled triangle of stars
 n=int(input())
 for i in range(0,n+1):

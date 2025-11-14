a9c83a2e06545a6ef245942aefe092cb604837bf
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-14 12:02:14 -0500

    nov

--- loops.py
+++ loops.py
@@ -414,8 +414,8 @@ while num > 0:
     if num % 2 == 0:
         num //= 2
     else:
-        num -= 1
-    steps += 1"""
+        num -= 1"""
+    steps += 1
 print(steps)
 
 # Number of Good Pairs

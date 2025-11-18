4ee3323acfb568fc812f9fae35d3d35be27eda17
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-18 13:14:50 -0500

    nov

--- list.py
+++ list.py
@@ -41,9 +41,9 @@ print(listc)
 #8 if reverse(descending order)
 listc.sort(reverse=True)
 print(listc)
-"""
+
 #9 squares in list
-llist=[1,2,3,4,5,6,7,8,9,10]
+llist=[1,2,3,4,5,6,7,8,9,10]"""
 for x in llist:
     i=0
     i=i*x

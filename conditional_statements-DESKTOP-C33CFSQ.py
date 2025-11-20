13ee482e1548818d25203a958d03827c5824405f
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-20 12:31:34 -0500

    nov

--- conditional_statements-DESKTOP-C33CFSQ.py
+++ conditional_statements-DESKTOP-C33CFSQ.py
@@ -2,8 +2,8 @@
 
 def friends_in_trouble(j_angry, s_angry):
     if(j_angry == True and s_angry == True):
-        return True"""
-    elif(j_angry == True and s_angry == False):
+        return True
+    elif(j_angry == True and s_angry == False):"""
         return False
 
 #2

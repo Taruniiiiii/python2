05eab66f5d33aecf00568bb60bc4d7c2d2c39c11
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-20 12:32:04 -0500

    nov

--- conditional_statements-DESKTOP-C33CFSQ.py
+++ conditional_statements-DESKTOP-C33CFSQ.py
@@ -3,8 +3,8 @@
 def friends_in_trouble(j_angry, s_angry):
     if(j_angry == True and s_angry == True):
         return True
-    elif(j_angry == True and s_angry == False):"""
-        return False
+    elif(j_angry == True and s_angry == False):
+        return False"""
 
 #2
 def checkOddEven(x):

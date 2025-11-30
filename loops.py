77ebba9a572aa359755b413674497f458529666e
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-11-30 11:30:35 -0500

    nov

--- loops.py
+++ loops.py
@@ -392,8 +392,8 @@ num = 1 # starting number
 
 for i in range(1, n+1):  # rows
     for j in range(i):   # print i numbers in that row
-        print(num, end=" ")
-        num += 1"""
+        print(num, end=" ")"""
+        num += 1
     print()  # new line after each row
 
 #leetcode

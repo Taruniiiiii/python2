a8e46a1ef609b89709330072f77fef86f5984297
Taruni_Medishetty <medishettytaruni@gmail.com>
2025-12-09 13:11:34 -0500

    dec

--- conditional_statements.py
+++ conditional_statements.py
@@ -78,8 +78,8 @@ else:
 condition=str(input("Patient condition(critical/serious/normal): "))
 if condition=="critical":
     print("icu bed")
-elif condition=="serious":
-    print("special ward")"""
+elif condition=="serious":"""
+    print("special ward")
 elif condition=="normal":
     print("general ward")
 else:

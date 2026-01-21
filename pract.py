c16f8cd512cb4579a6f8db1f1a4a1fa5bd441e6e
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-01-21 13:21:51 -0500

    jan

---
+++ pract.py
@@ -0,0 +1,27 @@
+#string palindrome
+def is_palindrome(s):
+  reverse=""
+  for ch in s:
+    reverse=ch+reverse
+  if s==reverse:
+    print("palindrome")
+  else:
+    print("not palindrome")
+  
+text="madam"
+print(is_palindrome(text))
+
+n=int(input())
+fact=1
+for i in range(1,n+1):#takes values from 1 to n+1
+  fact=fact*i#  1*1,1*2,2*3,6*4,24*5
+print(fact)
+
+#prime or not
+n=int(input())
+for i in range(2, n):
+    if n % i == 0:
+        print("not prime")
+        break
+else:
+    print("prime")

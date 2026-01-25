79158b97ba85b768b6d3348bb47bda3307dd8283
Taruni_Medishetty <158485758+Taruniiiiii@users.noreply.github.com>
2026-01-25 23:49:06 +0530

    Update pract.py

--- pract.py
+++ pract.py
@@ -21,7 +21,7 @@ print(fact)
 n=int(input())
 for i in range(2, n):
     if n % i == 0:
-        print("not prime")"""
-        break
+        print("not prime")
+        break"""
 else:
     print("prime")

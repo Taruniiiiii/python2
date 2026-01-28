c266578b0d7f23216d60dbff1d7d252afb79efbe
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-01-27 22:58:00 -0500

    jan

--- pract.py
+++ pract.py
@@ -1,4 +1,4 @@
-#string palindrome
+"""#string palindrome
 def is_palindrome(s):
   reverse=""
   for ch in s:
@@ -25,3 +25,55 @@ for i in range(2, n):
         break
 else:
     print("prime")
+    
+data = input()
+##
+if data.isdigit():
+    print("Numeric")
+else:
+    print("Not numeric")
+##
+logs = ["error", "info", "error", "warning", "error"]
+count = 0
+
+for log in logs:
+    if log == "error":
+        count += 1
+
+print("Error count:", count)
+##
+amount = float(input("Enter amount: "))
+
+if amount > 5000:
+    discount = amount * 0.1
+else:
+    discount = 0
+
+print("Final bill:", amount - discount)
+##
+print("\n2. Which keyword is used to define a function in Python?")
+print("a) def\nb) function\nc) fun\nd) define")
+ans = input("Your answer: ")
+if ans.lower()=="a":
+    score+=1
+
+##
+n = int(input())
+temp = n
+order = len(str(n))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** order
+    temp //= 10
+
+print("Armstrong" if sum == n else "Not Armstrong")
+"""
+##28 duplicates
+sen=input("enter a sentence").split()
+unique=[]
+for w in sen:
+  if w not in unique:
+      unique.append(w)
+print(" ".join(unique))

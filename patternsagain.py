bc417c5889741df7c68fe48c2d1ab264c77fd3f4
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-01-29 13:33:46 -0500

    jan

--- patternsagain.py
+++ patternsagain.py
@@ -1,7 +1,23 @@
 #29
-
+"""
 for i in range(5):
     for j in range(5):
         if j<=5:
             print("*",end=" ")
+    print()
+
+* * * * * 
+* * * * * 
+* * * * *
+* * * * *
+* * * * *"""
+
+#hollo square
+n=4
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
     print()

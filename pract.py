872ee9191f168a6b6357347eae31cafc3f9e1648
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-01-30 12:33:32 -0500

    jan

--- pract.py
+++ pract.py
@@ -145,3 +145,19 @@ class Solution(object):
     def isAnagram(self, s, t):
         return sorted(s)==sorted(t)
         
+##
+class Solution(object):
+    def sortedSquares(self, nums):
+        n=len(nums)
+        res = [0] * n
+        left,right=0,n-1
+        pos=n-1
+        while left<=right:
+            if nums[left]*nums[left]>nums[right]*nums[right]:
+                res[pos]=nums[left]*nums[left]
+                left+=1
+            else:
+                res[pos]=nums[right]*nums[right]
+                right-=1
+            pos-=1
+        return res

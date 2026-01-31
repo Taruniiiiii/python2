0ded9adeac34dbc859e87934e2ae869c3da4c614
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-01-31 12:27:38 -0500

    jan

--- pract.py
+++ pract.py
@@ -169,3 +169,18 @@ class Solution(object):
         else:
             return False
         
+##
+class Solution(object):
+    def fizzBuzz(self, n):
+        res=[]
+        for i in range(1,n+1):
+            
+            if i%3==0 and i%5==0:
+                res.append("FizzBuzz")
+            elif i%3==0:
+                res.append("Fizz")
+            elif i%5==0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+        return res

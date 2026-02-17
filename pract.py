ac04328540e7754f88e8bfcb862f30bd5255cfe7
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-02-17 12:14:56 -0500

    feb

--- pract.py
+++ pract.py
@@ -282,3 +282,8 @@ class Solution:
                 left+=1
                 right-=1
         return ''.join(s)
+##
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        result=int(num1)*int(num2)
+        return str(result)

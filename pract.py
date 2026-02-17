10f03a3af16bc00a68e978e39d9fe379283b7934
Taruni_Medishetty <medishettytaruni@gmail.com>
2026-02-17 11:42:35 -0500

    feb

--- pract.py
+++ pract.py
@@ -267,3 +267,18 @@ class Solution:
         return True
             
         
+##leet
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s=list(s)
+        left,right=0,len(s)-1
+        while left<right:
+            if not s[left].isalpha():
+                left+=1
+            elif not s[right].isalpha():
+                right-=1
+            else:
+                s[left],s[right]=s[right],s[left]
+                left+=1
+                right-=1
+        return ''.join(s)

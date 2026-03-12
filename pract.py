f1a619193814cc31d51cdcb141175c376e6b3216
Taruni Medishetty <medishettytaruni@gmail.com>
2026-03-12 23:17:08 +0530

    mar

--- pract.py
+++ pract.py
@@ -454,3 +454,15 @@ def is_prime(n):
 for num in range(2,101):
     if is_prime(num):
         print(num)
+        
+#leet
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums=list(set(nums))
+        nums.sort()
+        if len(nums)>=3:
+            return nums[-3]
+        else:
+            return nums[-1]
+        
+        
